c66eb452f42413839bb49e05526029d6cdc5ccf4
Lina Dam <linadam.86@gmail.com>
2023-10-10 12:36:30 +0200

    first changes

---
+++ index.js
@@ -0,0 +1,7 @@
+const URL = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=0043cddf9b5d097e63449cd8f3a64805";
+
+fetch (URL).then((response) => {
+    console.log(URL);
+}
+)
+
